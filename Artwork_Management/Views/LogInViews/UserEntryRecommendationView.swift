a8987a839db278e344a98b642f2302c182acea53
Kensuke Nakagawa <kennsuke242424@gmail.com>
2023-05-19 15:54:58 +0900

    アカウント登録成功時、シートを閉じる

--- Artwork_Management/Views/LogInViews/UserEntryRecommendationView.swift
+++ Artwork_Management/Views/LogInViews/UserEntryRecommendationView.swift
@@ -121,6 +121,9 @@ struct UserEntryRecommendationView: View {
                    isPresented: $logInVM.showAccountLinkAlert) {
                 Button("OK") {
                     logInVM.showAccountLinkAlert.toggle()
+                    if logInVM.resultAccountLink {
+                        isShow.toggle()
+                    }
                 }
             } message: {
                 if logInVM.resultAccountLink {
