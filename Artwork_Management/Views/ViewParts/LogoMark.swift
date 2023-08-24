13ea0f7a9f39711ab8c91ef05c1658c25317a3da
Kensuke Nakagawa <kennsuke242424@gmail.com>
2023-08-24 10:28:51 +0900

    ロゴ調整

--- Artwork_Management/Views/ViewParts/LogoMark.swift
+++ Artwork_Management/Views/ViewParts/LogoMark.swift
@@ -12,21 +12,22 @@ struct LogoMark: View {
 
         VStack {
 
-            Image(systemName: "cube.transparent")
+//            Image(systemName: "cube.transparent")
+            Image("unico_logo_text_white2")
                 .resizable()
                 .scaledToFit()
                 .foregroundColor(.white.opacity(0.7))
-                .opacity(0.7)
-                .frame(width: 150, height: 150)
+                .opacity(0.9)
+                .frame(width: 200, height: 200)
                 .padding()
 
-            Text("unico")
-                .tracking(25)
-                .font(.title3)
-                .foregroundColor(.white.opacity(0.6))
-                .opacity(0.6)
-                .fontWeight(.heavy)
-                .offset(x: 10)
+//            Text("unico")
+//                .tracking(25)
+//                .font(.title3)
+//                .foregroundColor(.white.opacity(0.6))
+//                .opacity(0.6)
+//                .fontWeight(.heavy)
+//                .offset(x: 10)
         } // VStack
     } // body
 } // View
@@ -36,27 +37,31 @@ struct LargeLogoMark: View {
 
         VStack {
 
-            Image(systemName: "cube.transparent")
+//            Image(systemName: "cube.transparent")
+            Image("unico_logo_text_white2")
                 .resizable()
                 .scaledToFit()
                 .foregroundColor(.white.opacity(0.7))
-                .opacity(0.7)
-                .frame(width: 150, height: 150)
+                .opacity(0.9)
+                .frame(width: 250, height: 250)
                 .padding()
-
-            Text("unico")
-                .tracking(25)
-                .font(.title3)
-                .foregroundColor(.white.opacity(0.6))
-                .opacity(0.6)
-                .fontWeight(.heavy)
-                .offset(x: 10)
+//
+//            Text("unico")
+//                .tracking(25)
+//                .font(.title3)
+//                .foregroundColor(.white.opacity(0.6))
+//                .opacity(0.6)
+//                .fontWeight(.heavy)
+//                .offset(x: 10)
         } // VStack
     } // body
 } // View
 
 struct LogoMark_Previews: PreviewProvider {
     static var previews: some View {
-        LogoMark()
+        ZStack {
+            Color.userBlue1
+            LogoMark()
+        }
     }
 }
