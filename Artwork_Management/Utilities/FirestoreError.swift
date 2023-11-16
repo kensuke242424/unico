b2571b7080c96b1ef2bdb844555c16fe769ce581
Kensuke Nakagawa <kennsuke242424@gmail.com>
2023-11-16 22:00:07 +0900

    FirestoreSerializable fetchメソッド追加

---
+++ Artwork_Management/Utilities/FirestoreError.swift
@@ -0,0 +1,8 @@
+//
+//  FirestoreError.swift
+//  Artwork_Management
+//
+//  Created by Kensuke Nakagawa on 2023/11/16.
+//
+
+import Foundation
--- Artwork_Management/Utilities/FirestoreSerializable.swift
+++ Artwork_Management/Utilities/FirestoreSerializable.swift
@@ -5,7 +5,28 @@
 //  Created by Kensuke Nakagawa on 2023/11/16.
 //
 
+import FirebaseFirestore
+
 protocol FirestoreSerializable {
     var id: String { get }
-    func firestorePath() -> FirestorePath
+    static func firestorePath() -> FirestorePath
+}
+
+extension FirestoreSerializable {
+
+    static func fetch<T: FirestoreSerializable & Decodable>(withId id: String) async throws -> T {
+
+        do {
+            let teamDocument = try await Firestore.firestore()
+                .collection(firestorePath().collectionPath)
+                .document(id)
+                .getDocument()
+
+            let data = try teamDocument.data(as: T.self)
+            return data
+        } catch {
+            print("ERROR: データ取得失敗")
+            throw CustomError.fetch
+        }
+    }
 }
