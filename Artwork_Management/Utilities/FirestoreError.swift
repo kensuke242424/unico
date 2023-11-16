6c2d1bb8dfdb72e441a008e8daee2f2c3bc6b28c
Kensuke Nakagawa <kennsuke242424@gmail.com>
2023-11-16 22:02:42 +0900

    add FirestoreError カスタムエラー追加

--- Artwork_Management/Utilities/FirestoreError.swift
+++ Artwork_Management/Utilities/FirestoreError.swift
@@ -5,4 +5,19 @@
 //  Created by Kensuke Nakagawa on 2023/11/16.
 //
 
-import Foundation
+enum FirestoreError: Error {
+    case dataNotFound
+    case decodeError
+    case other(Error)
+
+    var localizedDescription: String {
+        switch self {
+        case .dataNotFound:
+            return "データが見つかりません。"
+        case .decodeError:
+            return "データのデコードに失敗しました。"
+        case .other(let error):
+            return error.localizedDescription
+        }
+    }
+}
