cc7797cc9d53d1724c96b5b5ac512fa933fe8f84
Kensuke Nakagawa <kennsuke242424@gmail.com>
2023-11-18 16:23:13 +0900

    #51 add SaveStorageImageType

--- Artwork_Management/Utilities/FirebaseStorageManager.swift
+++ Artwork_Management/Utilities/FirebaseStorageManager.swift
@@ -5,8 +5,47 @@
 //  Created by Kensuke Nakagawa on 2023/11/18.
 //
 
-import Foundation
+import UIKit.UIImage
+import FirebaseStorage
 
 class FirebaseStorageManager {
+    func uploadImage(_ imageType: SaveStorageImageType, _ image: UIImage?) async -> (url: URL?, filePath: String?) {
 
+        guard let imageData = image?.jpegData(compressionQuality: 0.8) else {
+            return (url: nil, filePath: nil)
+        }
+
+        do {
+            let storage = Storage.storage()
+            let reference = storage.reference()
+            let filePath = "\(imageType.storageFilePath)/\(Date()).jpeg"
+            let imageRef = reference.child(filePath)
+            _ = try await imageRef.putDataAsync(imageData)
+            let url = try await imageRef.downloadURL()
+
+            return (url: url, filePath: filePath)
+        } catch {
+            return (url: nil, filePath: nil)
+        }
+    }
+}
+
+enum SaveStorageImageType {
+    case user(userId: String)
+    case team(teamId: String)
+    case item(teamId: String)
+    case myBackground(userId: String)
+
+    var storageFilePath: String {
+        switch self {
+        case .user(let userId):
+            return "users/\(userId)"
+        case .team(let teamId):
+            return "teams/\(teamId)"
+        case .item(let teamId):
+            return "teams/\(teamId)/items"
+        case .myBackground(let userId):
+            return "users/\(userId)/myBackgrounds"
+        }
+    }
 }
